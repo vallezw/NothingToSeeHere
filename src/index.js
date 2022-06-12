a8cfb4ad80a3180dcb291dc633cdad0ebbec0f45
Valentin Zwerschke <v.zwerschke@mail.de>
2022-06-12 15:18:58 +0200

    Refactor project and make v2.0

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Typical from "react-typical";
+
+
+ReactDOM.render(
+  <React.StrictMode >
+      <div className="container">
+        <header className="header">
+          <h1>404</h1>
+          <Typical steps={["Nothing to see here"]} wrapper="p" className="typical"/>
+        </header>
+      </div>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
